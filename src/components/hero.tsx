d237e486a202bb96ca763f10be3d8e56cac35204
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2025-04-21 16:34:26 +0100

    update: video play on safari

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,23 +1,36 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Button } from "./ui/button";
 import { Calendar, X } from "lucide-react";
 import FormModal from "./formModal";
 
+
 export default function Hero() {
   const [showForm, setShowForm] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if(videoRef.current){
+      videoRef.current.muted = true
+      videoRef.current.play().catch()
+    }
+  }, [])
+
   return (
     <section className="relative h-[700px] md:h-screen flex flex-col items-end justify-end pb-20 px-8 md:px-16 w-full">
       <video
+        ref={videoRef}
         autoPlay
         loop
         muted
         playsInline
+        webkit-playsinline="true"
+        preload="auto"
         className="absolute top-0 left-0 w-full h-full object-cover filter grayscale"
       >
-        <source src="/videos/background-video.webm" type="video/webm"/>
         <source src="/videos/background-video.mp4" type="video/mp4"/>
+        <source src="/videos/background-video.webm" type="video/webm"/>
         Erro ao carregar o vídeo.
       </video>
 
