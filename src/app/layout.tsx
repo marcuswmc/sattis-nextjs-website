ee3e346c2ceec83914382fda325ab0bd60fe02d8
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2025-05-15 13:21:42 +0100

    feat: Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/header";
 import AppointmentsProviderWrapper from "@/hooks/appointmentsProviderWrapper";
+import { Analytics } from "@vercel/analytics/next"
 
 import { Toaster } from "sonner";
 
@@ -28,6 +29,7 @@ export default function RootLayout({
           <Header />
           {children}
           <Toaster />
+          <Analytics/>
         </AppointmentsProviderWrapper>
       </body>
     </html>
